bcff7ae847cda3d3916159ed7d94f6e6def6a80a
Marcelo Junior <marcelojunior.tech@gmail.com>
2021-12-06 19:16:02 -0300

    Adicionado Configuração Personalizada do RabbitMQ e init.sh personalizado.

---
+++ init.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+apt -yqq install gcc rabbitmq-server
+cp -i /home/site/wwwroot/rabbitmq.conf /etc/rabbitmq/rabbitmq.conf
+service rabbitmq-server start
+
+cd /home/site/wwwroot
+pip install -r requirements.txt
+
+celery multi start w1 -A achadosepedidos -l info
+
+#gunicorn --bind=0.0.0.0 --timeout 600 achadosepedidos.wsgi
+gunicorn --workers 4 --threads 2 --bind=0.0.0.0 --timeout 600 --access-logfile '/tmp/django-access.log' --error-logfile '/tmp/django-error.log' achadosepedidos.wsgi
