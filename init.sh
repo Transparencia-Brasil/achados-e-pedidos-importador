d3cdab2f75bcf1999f22a4d79da8f60d93cd8860
Paulo Campos <paulo.campos@studiocuboweb.com.br>
2021-12-07 15:14:35 -0300

    teste de deploy

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 apt -yqq install gcc rabbitmq-server
-#Copia a nova configuração do rabbitmq 
+#Copia a nova configuração do rabbitmq teste de VM
 cp -i /home/site/wwwroot/rabbitmq.conf /etc/rabbitmq/rabbitmq.conf
 service rabbitmq-server start
 
